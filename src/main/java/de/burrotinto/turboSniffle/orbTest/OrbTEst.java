a852d35ccf413d45a2fef88ca99270cffd81b97a
fklinger <derduke@posteo.de>
2021-06-10 15:44:54 +0200

    ORB Test

---
+++ src/main/java/de/burrotinto/turboSniffle/orbTest/OrbTEst.java
@@ -0,0 +1,17 @@
+//package de.burrotinto.turboSniffle.orbTest;
+//
+//import org.opencv.core.Mat;
+//import org.opencv.core.MatOfKeyPoint;
+//import org.opencv.features2d.ORB;
+//
+//public class OrbTEst {
+//
+//
+//    public static void main(String[] args) {
+//
+//        ORB orb = ORB.create();
+//        MatOfKeyPoint keypoints = new MatOfKeyPoint();
+//        Mat descriptors = new Mat();
+//        orb.detectAndCompute(img, new Mat(), keypoints, descriptors);
+//    }
+//}
