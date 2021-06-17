57f3a54212e65d20ba5cf52355ee0639d0fd9a2f
Florian Klinger <derduke@posteo.de>
2021-06-17 06:20:30 +0200

    sobelCanny

--- src/main/java/de/burrotinto/turboSniffle/cessna172/LearnAutoencoder.java
+++ src/main/java/de/burrotinto/turboSniffle/cessna172/LearnAutoencoder.java
@@ -1,167 +1,167 @@
-package de.burrotinto.turboSniffle.cessna172;
-
-import de.burrotinto.turboSniffle.booleanAutoEncoder.BooleanAutoencoder;
-import de.burrotinto.turboSniffle.booleanAutoEncoder.BooleanAutoencoder2;
-import de.burrotinto.turboSniffle.booleanAutoEncoder.HammingDouble;
-import de.burrotinto.turboSniffle.cv.DisplayMainFrame;
-import de.burrotinto.turboSniffle.meters.MeasuringType;
-import de.burrotinto.turboSniffle.meters.gauge.CirceGaugeOnePointer;
-import lombok.val;
-import org.opencv.core.*;
-import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-
-public class LearnAutoencoder {
-    private final static double STEPS = 1;
-    private final static String AIRSPEED_FILE = "pointer.png";
-    private final static Size SIZE = new Size(512, 512);
-    private final BooleanAutoencoder autoencoder = new BooleanAutoencoder(128);
-    private Mat src;
-
-
-    public LearnAutoencoder() {
-
-//        learnAutoencoder2();
-
-
-        src = Imgcodecs.imread(AIRSPEED_FILE);
-        Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2GRAY);
-
-
-        LinkedList<Mat> list = new LinkedList<>();
-
+//package de.burrotinto.turboSniffle.cessna172;
 //
-//        for (int i = 0; i < 360; i++) {
-//            Mat dst = Mat.zeros(SIZE,src.type());
+//import de.burrotinto.turboSniffle.booleanAutoEncoder.BooleanAutoencoder;
+//import de.burrotinto.turboSniffle.booleanAutoEncoder.BooleanAutoencoder2;
+//import de.burrotinto.turboSniffle.booleanAutoEncoder.HammingDouble;
+//import de.burrotinto.turboSniffle.cv.DisplayMainFrame;
+//import de.burrotinto.turboSniffle.meters.MeasuringType;
+//import de.burrotinto.turboSniffle.meters.gauge.CirceGaugeOnePointer;
+//import lombok.val;
+//import org.opencv.core.*;
+//import org.opencv.highgui.HighGui;
+//import org.opencv.imgcodecs.Imgcodecs;
+//import org.opencv.imgproc.Imgproc;
 //
-//            val rotate = Imgproc.getRotationMatrix2D(new Point(SIZE.width / 2, SIZE.height / 2), i, 1.0);
-//            Imgproc.warpAffine(src, dst, rotate, SIZE);
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//public class LearnAutoencoder {
+//    private final static double STEPS = 1;
+//    private final static String AIRSPEED_FILE = "pointer.png";
+//    private final static Size SIZE = new Size(512, 512);
+//    private final BooleanAutoencoder autoencoder = new BooleanAutoencoder(128);
+//    private Mat src;
+//
+//
+//    public LearnAutoencoder() {
+//
+////        learnAutoencoder2();
+//
+//
+//        src = Imgcodecs.imread(AIRSPEED_FILE);
+//        Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2GRAY);
+//
+//
+//        LinkedList<Mat> list = new LinkedList<>();
+//
+////
+////        for (int i = 0; i < 360; i++) {
+////            Mat dst = Mat.zeros(SIZE,src.type());
+////
+////            val rotate = Imgproc.getRotationMatrix2D(new Point(SIZE.width / 2, SIZE.height / 2), i, 1.0);
+////            Imgproc.warpAffine(src, dst, rotate, SIZE);
+////            list.add(dst);
+////        }
+//
+//
+//        val drawing = Mat.zeros(SIZE, src.type());
+//        Imgproc.line(drawing, new Point(SIZE.width / 2, SIZE.height / 2), new Point(SIZE.width / 2, 20), new Scalar(255, 255, 255), 20);
+//
+//
+//        for (int i = 0; i < 360 * (1 / STEPS); i++) {
+//            Mat dst = Mat.zeros(SIZE, src.type());
+//
+//            val rotate = Imgproc.getRotationMatrix2D(new Point(SIZE.width / 2, SIZE.height / 2), i * STEPS, 1.0);
+//            Imgproc.warpAffine(drawing, dst, rotate, SIZE);
 //            list.add(dst);
 //        }
-
-
-        val drawing = Mat.zeros(SIZE, src.type());
-        Imgproc.line(drawing, new Point(SIZE.width / 2, SIZE.height / 2), new Point(SIZE.width / 2, 20), new Scalar(255, 255, 255), 20);
-
-
-        for (int i = 0; i < 360 * (1 / STEPS); i++) {
-            Mat dst = Mat.zeros(SIZE, src.type());
-
-            val rotate = Imgproc.getRotationMatrix2D(new Point(SIZE.width / 2, SIZE.height / 2), i * STEPS, 1.0);
-            Imgproc.warpAffine(drawing, dst, rotate, SIZE);
-            list.add(dst);
-        }
-
-        autoencoder.learn(list);
-
-
-        val s = Imgcodecs.imread("sixpacks/round1116089649.jpg");
-        val display = new DisplayMainFrame();
-        display.setMainframe(s);
-        display.addMeasuring(new Point(0, 0), (int) s.size().width, "TEST", MeasuringType.AnalogSimple, "");
-
-//        val c = Mat.zeros(display.getMeasuring("TEST").getDrawing().size(), display.getMeasuring("TEST").getDrawing().type());
 //
-//        LinkedList<MatOfPoint> points = new LinkedList<>();
-//        points.add(((CirceGaugeOnePointer) display.getMeasuring("TEST")).getPointer()[0].getContour());
+//        autoencoder.learn(list);
 //
-//        Imgproc.drawContours(c, points, 0, new Scalar(255, 255, 255), -1);
 //
-
-        val x = autoencoder.classify(s);
-
-
-        Mat cannyOutput = new Mat();
-        Imgproc.Canny(x, cannyOutput, 130, 130 * 2);
-
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
-        Imgproc.findContours(cannyOutput, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
-//        Mat xxx = Imgcodecs.imread("data/example/20210514_061025 - Kopie.jpg");
-//        Imgproc.drawContours(xxx, contours, 0, new Scalar(255, 255, 255));
-
-        System.out.printf(x.type() + "");
-        HighGui.imshow("src", x);
-//        HighGui.imshow("", c);
-//        HighGui.imshow("finding", xxx);
-        HighGui.waitKey();
-
-    }
-
-
-    private void learnAutoencoder2(){
-        src = Imgcodecs.imread(AIRSPEED_FILE);
-        Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2GRAY);
-
-
-        HashMap<Mat,HammingDouble> list = new HashMap<>();
-
-
-        val drawing = Mat.zeros(SIZE, src.type());
-        Imgproc.line(drawing, new Point(SIZE.width / 2, SIZE.height / 2), new Point(SIZE.width/2, SIZE.height- 20), new Scalar(255, 255, 255), 20);
-
-
-        for (double i = 0; i < 360 * (1 / STEPS); i++) {
-            Mat dst = Mat.zeros(SIZE, src.type());
-
-            val rotate = Imgproc.getRotationMatrix2D(new Point(SIZE.width / 2, SIZE.height / 2), i * STEPS, 1.0);
-            Imgproc.warpAffine(drawing, dst, rotate, SIZE);
-            list.put(dst,new HammingDouble(i * STEPS));
-        }
-
-        BooleanAutoencoder2<HammingDouble> autoencoder = new BooleanAutoencoder2<>(64*64);
-
-        autoencoder.learn(list);
-
-
-        val s = Imgcodecs.imread("1621749290008.jpg");
-        val display = new DisplayMainFrame();
-        display.setMainframe(s);
-        display.addMeasuring(new Point(0, 0), (int) s.size().width, "TEST", MeasuringType.AnalogSimple, "");
-
-        HighGui.imshow("yxcyc", s);
-        HighGui.waitKey();
-
-        val c = Mat.zeros(display.getMeasuring("TEST").getSize(), 0);
-//        LinkedList<MatOfPoint> points = new LinkedList<>();
-//        points.add(((CirceGaugeOnePointer) display.getMeasuring("TEST")).getPointer()[0].getContour());
+//        val s = Imgcodecs.imread("sixpacks/round1116089649.jpg");
+//        val display = new DisplayMainFrame();
+//        display.setMainframe(s);
+//        display.addMeasuring(new Point(0, 0), (int) s.size().width, "TEST", MeasuringType.AnalogSimple, "");
 //
-//        Imgproc.drawContours(c, points, 0, new Scalar(255, 255, 255), -1);
+////        val c = Mat.zeros(display.getMeasuring("TEST").getDrawing().size(), display.getMeasuring("TEST").getDrawing().type());
+////
+////        LinkedList<MatOfPoint> points = new LinkedList<>();
+////        points.add(((CirceGaugeOnePointer) display.getMeasuring("TEST")).getPointer()[0].getContour());
+////
+////        Imgproc.drawContours(c, points, 0, new Scalar(255, 255, 255), -1);
+////
 //
-
-        val x = autoencoder.doAutoencoderStuff(s);
-        System.out.println(autoencoder.encode(s).value);
-
-
-        Mat cannyOutput = new Mat();
-        Imgproc.Canny(x, cannyOutput, 130, 130 * 2);
-
-        List<MatOfPoint> contours = new ArrayList<>();
-        Mat hierarchy = new Mat();
-        Imgproc.findContours(cannyOutput, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
-//        Mat xxx = Imgcodecs.imread("data/example/20210514_061025 - Kopie.jpg");
-//        Imgproc.drawContours(xxx, contours, 0, new Scalar(255, 255, 255));
-
-        System.out.printf(x.type() + "");
-        HighGui.imshow("src", x);
-//        HighGui.imshow("", c);
-//        HighGui.imshow("finding", xxx);
-        HighGui.waitKey();
-    }
-    public static void main(String[] args) {
-
-//        System.out.println(new HammingDouble(1.0).getHammingDistanzTo(new HammingDouble(100.0)));
-
-        nu.pattern.OpenCV.loadLocally();
-
-        new LearnAutoencoder();
-    }
-}
+//        val x = autoencoder.classify(s);
+//
+//
+//        Mat cannyOutput = new Mat();
+//        Imgproc.Canny(x, cannyOutput, 130, 130 * 2);
+//
+//        List<MatOfPoint> contours = new ArrayList<>();
+//        Mat hierarchy = new Mat();
+//        Imgproc.findContours(cannyOutput, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+//
+////        Mat xxx = Imgcodecs.imread("data/example/20210514_061025 - Kopie.jpg");
+////        Imgproc.drawContours(xxx, contours, 0, new Scalar(255, 255, 255));
+//
+//        System.out.printf(x.type() + "");
+//        HighGui.imshow("src", x);
+////        HighGui.imshow("", c);
+////        HighGui.imshow("finding", xxx);
+//        HighGui.waitKey();
+//
+//    }
+//
+//
+//    private void learnAutoencoder2(){
+//        src = Imgcodecs.imread(AIRSPEED_FILE);
+//        Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2GRAY);
+//
+//
+//        HashMap<Mat,HammingDouble> list = new HashMap<>();
+//
+//
+//        val drawing = Mat.zeros(SIZE, src.type());
+//        Imgproc.line(drawing, new Point(SIZE.width / 2, SIZE.height / 2), new Point(SIZE.width/2, SIZE.height- 20), new Scalar(255, 255, 255), 20);
+//
+//
+//        for (double i = 0; i < 360 * (1 / STEPS); i++) {
+//            Mat dst = Mat.zeros(SIZE, src.type());
+//
+//            val rotate = Imgproc.getRotationMatrix2D(new Point(SIZE.width / 2, SIZE.height / 2), i * STEPS, 1.0);
+//            Imgproc.warpAffine(drawing, dst, rotate, SIZE);
+//            list.put(dst,new HammingDouble(i * STEPS));
+//        }
+//
+//        BooleanAutoencoder2<HammingDouble> autoencoder = new BooleanAutoencoder2<>(64*64);
+//
+//        autoencoder.learn(list);
+//
+//
+//        val s = Imgcodecs.imread("1621749290008.jpg");
+//        val display = new DisplayMainFrame();
+//        display.setMainframe(s);
+//        display.addMeasuring(new Point(0, 0), (int) s.size().width, "TEST", MeasuringType.AnalogSimple, "");
+//
+//        HighGui.imshow("yxcyc", s);
+//        HighGui.waitKey();
+//
+//        val c = Mat.zeros(display.getMeasuring("TEST").getSize(), 0);
+////        LinkedList<MatOfPoint> points = new LinkedList<>();
+////        points.add(((CirceGaugeOnePointer) display.getMeasuring("TEST")).getPointer()[0].getContour());
+////
+////        Imgproc.drawContours(c, points, 0, new Scalar(255, 255, 255), -1);
+////
+//
+//        val x = autoencoder.doAutoencoderStuff(s);
+//        System.out.println(autoencoder.encode(s).value);
+//
+//
+//        Mat cannyOutput = new Mat();
+//        Imgproc.Canny(x, cannyOutput, 130, 130 * 2);
+//
+//        List<MatOfPoint> contours = new ArrayList<>();
+//        Mat hierarchy = new Mat();
+//        Imgproc.findContours(cannyOutput, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+//
+////        Mat xxx = Imgcodecs.imread("data/example/20210514_061025 - Kopie.jpg");
+////        Imgproc.drawContours(xxx, contours, 0, new Scalar(255, 255, 255));
+//
+//        System.out.printf(x.type() + "");
+//        HighGui.imshow("src", x);
+////        HighGui.imshow("", c);
+////        HighGui.imshow("finding", xxx);
+//        HighGui.waitKey();
+//    }
+//    public static void main(String[] args) {
+//
+////        System.out.println(new HammingDouble(1.0).getHammingDistanzTo(new HammingDouble(100.0)));
+//
+//        nu.pattern.OpenCV.loadLocally();
+//
+//        new LearnAutoencoder();
+//    }
+//}
--- src/main/java/de/burrotinto/turboSniffle/meters/gauge/Gauge.java
+++ src/main/java/de/burrotinto/turboSniffle/meters/gauge/Gauge.java
@@ -1,16 +1,7 @@
 package de.burrotinto.turboSniffle.meters.gauge;
 
 import lombok.val;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.RotatedRect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.core.*;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -27,33 +18,41 @@ public class Gauge {
         Imgproc.cvtColor(input, greyInput, Imgproc.COLOR_BGR2GRAY);
 //        Imgproc.resize(greyInput,greyInput,new Size(256,256));
 
-        val canny = getEdgeDedectionCanny(greyInput,85);
-        val sobel = getSobel(greyInput,85);
-        HighGui.imshow("CANNY",canny);
-        HighGui.imshow("SOBEL",sobel);
+        val canny = getEdgeDedectionCanny(greyInput, 85);
+        val sobel = getEdgeDedectionSobel(greyInput, 85);
+        val both =  getEdgeDedectionSobelAndCanny(greyInput, 85);
+//        HighGui.imshow("CANNY", canny);
+//        HighGui.imshow("SOBEL", sobel);
+        HighGui.imshow("BOTH",both);
 
 
-
-        Mat out = new Mat(sobel.size(),sobel.type());
+        Mat out = new Mat(sobel.size(), sobel.type());
         val sobelLines = getLineDedection(sobel);
         val cannyLines = getLineDedection(canny);
+        val bothLines = getLineDedection(both);
 
-        for (int x = 0; x < sobelLines.rows(); x++) {
-            double[] l = sobelLines.get(x, 0);
-            Imgproc.line(out, new Point(l[0], l[1]), new Point(l[2], l[3]), new Scalar(255, 255, 255), 3, Imgproc.LINE_8, 0);
-        }
+//        for (int x = 0; x < sobelLines.rows(); x++) {
+//            double[] l = sobelLines.get(x, 0);
+//            Imgproc.line(out, new Point(l[0], l[1]), new Point(l[2], l[3]), new Scalar(255, 255, 255), 3, Imgproc.LINE_8, 0);
+//        }
 
 //        HighGui.imshow("SOBEL_LINES",out);
 //        HighGui.waitKey();
 
 
-        Imgproc.ellipse(canny, getGreatestElipse(removeLines(canny,cannyLines)),new Scalar(255,255,255),10);
-        Imgproc.ellipse(sobel, getGreatestElipse(removeLines(sobel,sobelLines)),new Scalar(255,255,255),10);
-        HighGui.imshow("CANNY_e",canny);
-        HighGui.imshow("SOBEL_e",sobel);
+        val bothWithOutLines = removeLines(both,bothLines);
+        Imgproc.ellipse(canny, getGreatestElipse(removeLines(canny, cannyLines)), new Scalar(255, 255, 255), 10);
+        Imgproc.ellipse(sobel, getGreatestElipse(removeLines(sobel, sobelLines)), new Scalar(255, 255, 255), 10);
+//        Imgproc.ellipse(both, getGreatestElipse( both), new Scalar(255, 255, 255), 10);
 
-        HighGui.imshow("trans Sobel",transponiere(input,getGreatestElipse(removeLines(sobel,sobelLines))));
-        HighGui.imshow("trans Canny",transponiere(input,getGreatestElipse(removeLines(canny,cannyLines))));
+
+//        HighGui.imshow("CANNY_e", canny);
+//        HighGui.imshow("SOBEL_e", sobel);
+        HighGui.imshow("BOTH_e",bothWithOutLines);
+
+//        HighGui.imshow("trans Sobel", transponiere(input, getGreatestElipse(removeLines(sobel, sobelLines))));
+//        HighGui.imshow("trans Canny", transponiere(input, getGreatestElipse(removeLines(canny, cannyLines))));
+        HighGui.imshow("trans BOTH", transponiere(input, getGreatestElipse(bothWithOutLines)));
 //                HighGui.imshow("trans Sobel",transponiere(input,getGreatestElipse(sobel)));
 //        HighGui.imshow("trans Canny",transponiere(input,getGreatestElipse(canny)));
 
@@ -61,12 +60,7 @@ public class Gauge {
     }
 
 
-
-
-
-
-
-   public static Mat getSobel(Mat input,int threshold){
+    public static Mat getEdgeDedectionSobel(Mat input, int threshold) {
         // First we declare the variables we are going to use
         Mat grad = new Mat();
         int scale = 1;
@@ -76,21 +70,21 @@ public class Gauge {
         Mat grad_x = new Mat(), grad_y = new Mat();
         Mat abs_grad_x = new Mat(), abs_grad_y = new Mat();
 
-        Imgproc.Sobel( input, grad_x, ddepth, 1, 0, 3, scale, delta, Core.BORDER_DEFAULT );
-        Imgproc.Sobel( input, grad_y, ddepth, 0, 1, 3, scale, delta, Core.BORDER_DEFAULT );
+        Imgproc.Sobel(input, grad_x, ddepth, 1, 0, 3, scale, delta, Core.BORDER_DEFAULT);
+        Imgproc.Sobel(input, grad_y, ddepth, 0, 1, 3, scale, delta, Core.BORDER_DEFAULT);
 
         // converting back to CV_8U
-        Core.convertScaleAbs( grad_x, abs_grad_x );
-        Core.convertScaleAbs( grad_y, abs_grad_y );
-        Core.addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+        Core.convertScaleAbs(grad_x, abs_grad_x);
+        Core.convertScaleAbs(grad_y, abs_grad_y);
+        Core.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
 
-        val out = new Mat(grad.size(),grad.type());
+        val out = new Mat(grad.size(), grad.type());
         for (int i = 0; i < grad.rows(); i++) {
             for (int j = 0; j < grad.cols(); j++) {
-                if(grad.get(i,j)[0]> threshold){
-                    out.put(i,j,255.0,255.0,255.0);
+                if (grad.get(i, j)[0] > threshold) {
+                    out.put(i, j, 255.0, 255.0, 255.0);
                 } else {
-                    out.put(i,j,0,0,0);
+                    out.put(i, j, 0, 0, 0);
                 }
 
             }
@@ -98,16 +92,33 @@ public class Gauge {
         return out;
     }
 
-    public static Mat getEdgeDedectionCanny(Mat mat, int threshold){
+    public static Mat getEdgeDedectionCanny(Mat mat, int threshold) {
         //Canny and Contours finding
         Mat cannyOutput = new Mat();
         Imgproc.Canny(mat, cannyOutput, threshold, threshold * 2);
         return cannyOutput;
     }
 
-   public static Mat getLineDedection(Mat mat) {
+    public static Mat getEdgeDedectionSobelAndCanny(Mat mat, int threshold) {
+        val sobel = getEdgeDedectionSobel(mat, threshold);
+        val cany = getEdgeDedectionCanny(mat, threshold);
+        val out = new Mat(mat.size(), sobel.type());
+
+        for (int i = 0; i < out.rows(); i++) {
+            for (int j = 0; j < out.cols(); j++) {
+                val s = sobel.get(i, j);
+                val c = cany.get(i, j);
+                if (Arrays.stream(s).sum() == Arrays.stream(c).sum()) {
+                    out.put(i, j, s);
+                }
+            }
+        }
+        return out;
+    }
+
+    public static Mat getLineDedection(Mat mat) {
         Mat linesP = new Mat(); // will hold the results of the detection
-        Imgproc.HoughLinesP(mat, linesP, 1, Math.PI / 180, 255 / 4, mat.size().height/4.0, mat.size().height/25.5); // runs the actual detection
+        Imgproc.HoughLinesP(mat, linesP, 1, Math.PI / 180, 255 / 4, mat.size().height / 4.0, mat.size().height / 25.5); // runs the actual detection
 
         return linesP;
     }
@@ -118,14 +129,10 @@ public class Gauge {
         var hierarchy = new Mat();
         Imgproc.findContours(edgeDetected, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        //Check und initialisierung der AnzeigeDabei wer
         RotatedRect[] minEllipse = new RotatedRect[contours.size()];
 
         int indexDisplay = 0;
 
-
-
-        val maxQuadrat = new Rect(0,0,edgeDetected.width(),edgeDetected.height());
 
         for (int i = 0; i < contours.size(); i++) {
             minEllipse[i] = new RotatedRect();
@@ -134,7 +141,7 @@ public class Gauge {
             }
 
 
-            val radius = (int) Math.max(minEllipse[i].size.width,minEllipse[i].size.height)/2;
+            val radius = (int) Math.max(minEllipse[i].size.width, minEllipse[i].size.height) / 2;
 
             if (minEllipse[i].size.area() > minEllipse[indexDisplay].size.area()
                     && minEllipse[i].center.x - radius >= 0
@@ -155,21 +162,21 @@ public class Gauge {
         Mat out = mat.clone();
         for (int x = 0; x < lines.rows(); x++) {
             double[] l = lines.get(x, 0);
-            Imgproc.line(out, new Point(l[0], l[1]), new Point(l[2], l[3]), new Scalar(0, 0, 255), 5, Imgproc.LINE_8, 0);
+            Imgproc.line(out, new Point(l[0], l[1]), new Point(l[2], l[3]), new Scalar(0, 0, 0), 2, Imgproc.LINE_8, 0);
         }
         return out;
     }
 
-    public static Mat transponiere(Mat mat, RotatedRect ellipse){
+    public static Mat transponiere(Mat mat, RotatedRect ellipse) {
         Point[] pts = new Point[4];
         ellipse.points(pts);
         return transponiere(mat, Arrays.stream(pts).collect(Collectors.toList()));
     }
-    public static Mat transponiere(Mat mat, List<Point> corner){
+
+    public static Mat transponiere(Mat mat, List<Point> corner) {
         //        https://stackoverflow.com/questions/40688491/opencv-getperspectivetransform-and-warpperspective-java
 
         //     https://github.com/badlogic/opencv-fun/blob/master/src/pool/tests/PerspectiveTransform.java
-
 
 
         List<Point> target = new ArrayList<Point>();
@@ -185,11 +192,12 @@ public class Gauge {
         Mat invTrans = Imgproc.getPerspectiveTransform(targetMat, cornersMat);
         Mat proj = new Mat();
         Imgproc.warpPerspective(mat, proj, trans, new Size(mat.cols(), mat.rows()));
-        Imgproc.resize(proj,proj,new Size(512,512));
+        Imgproc.resize(proj, proj, new Size(512, 512));
         return proj;
     }
+
     public static void main(String[] args) {
         nu.pattern.OpenCV.loadLocally();
-        new Gauge(Imgcodecs.imread("data/example/li.png"));
+        new Gauge(Imgcodecs.imread("data/example/temp.jpg"));
     }
 }
