00129a1c1ef4219cac6aee38ae89ebaa11f76cee
Florian Klinger <derduke@posteo.de>
2021-09-07 23:23:59 +0200

    Schwellenwerterkennung anhand einer Texterkennung

--- src/main/java/de/burrotinto/turboSniffle/meters/gauge/GarminG1000.java
+++ src/main/java/de/burrotinto/turboSniffle/meters/gauge/GarminG1000.java
@@ -1,7 +1,9 @@
 package de.burrotinto.turboSniffle.meters.gauge;
 
+import de.burrotinto.turboSniffle.cv.Helper;
 import de.burrotinto.turboSniffle.cv.Pair;
 import de.burrotinto.turboSniffle.cv.TextDedection;
+import de.burrotinto.turboSniffle.meters.gauge.impl.HeatMap;
 import lombok.SneakyThrows;
 import lombok.val;
 import org.opencv.core.*;
@@ -9,112 +11,118 @@ import org.opencv.highgui.HighGui;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.utils.Converters;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class GarminG1000 {
     //Bei einem 12" Display ist die Länge 9,6" und die höhe 7,2" entsprich 106,666 DPI
     public static final Size SIZE = new Size(1024, 768);
+    public static final Point POINT_OF_TRUST = new Point(460, 586);
+    private static final Rect KURSKREISEL_POS = new Rect(new Point(252, 335), new Point(678, 747));
     private static TextDedection textDedection = new TextDedection(TextDedection.ENGRESTRICT_BEST_INT, 107);
-    private Mat g1000;
+    private Mat g1000, g1000TextOptimiert;
+    private Point pot;
 
+    private Rect altimeterUP = new Rect(new Point(723, 113), new Point(788, 250));
+    private Rect altimeterDOWN = new Rect(new Point(723, 318), new Point(788, 455));
+    private Rect altimeter = new Rect(new Point(altimeterUP.x, altimeterUP.y),
+            new Point(altimeterDOWN.x + altimeterDOWN.width, altimeterDOWN.y + altimeterDOWN.height));
 
-    private final static Rect altimeter = new Rect(new Point(700, 70), new Point(810, 450));
+    private int centerLine = 284;
 
     @SneakyThrows
     public GarminG1000(Mat src) {
+        g1000TextOptimiert = new Mat();
         g1000 = new Mat();
+
+        Imgproc.resize(src, g1000TextOptimiert, SIZE);
         Imgproc.resize(src, g1000, SIZE);
+
+        //Init textOptimierteVersion
         textDedection.addOptions("tessedit_char_whitelist", "01234567890");
+        g1000TextOptimiert = getBestThreshAndMat(g1000TextOptimiert).p2;
 
-        g1000 = getBestThreshAndMat(g1000).p2;
+        val cannyEdgeDetector = GaugeFactory.getCanny();
+        cannyEdgeDetector.setSourceImage((BufferedImage) HighGui.toBufferedImage(g1000));
+        cannyEdgeDetector.process();
 
-//        int i = 50;
-//        while (i <= SIZE.height || i <= SIZE.width) {
-//            if (i <= SIZE.height) {
-//                Imgproc.line(g1000, new Point(0, i), new Point(SIZE.width, i), Helper.BLACK);
-//            }
-//            if (i <= SIZE.width) {
-//                Imgproc.line(g1000, new Point(i, 0), new Point(i, SIZE.height), Helper.BLACK);
-//            }
-//            i += 50;
-//        }
+        HeatMap heatMap = new HeatMap(cannyEdgeDetector.getEdgeMat().submat(KURSKREISEL_POS));
+        pot = new Point(KURSKREISEL_POS.x + heatMap.getCenter().x, KURSKREISEL_POS.y + heatMap.getCenter().y);
+
+        Imgproc.drawMarker(g1000, pot, Helper.BLACK, 0, 1000);
 
 
-//        val cannyEdgeDetector = GaugeFactory.getCanny();
-//        cannyEdgeDetector.setSourceImage((BufferedImage) HighGui.toBufferedImage(g1000));
-//        cannyEdgeDetector.process();
-//
-//        Map<RotatedRect, Mat> map = new HashMap<>();
-//        ArrayList<MatOfPoint> contours = new ArrayList<>();
-//        Mat hierarchy = new Mat();
-//        Imgproc.findContours(cannyEdgeDetector.getEdgeMat(), contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-//
-//        HighGui.imshow("canny", cannyEdgeDetector.getEdgeMat());
-////        HighGui.waitKey();
-//
-//        for (int i = 0; i < contours.size(); i++) {
-//            val rect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
-//
-//            //Nur Konturen die eine Mindestgröße besitzen
-//            if (rect.size.area() > 1000) {
-////                Mat xxx = transformieren(g1000,rect,g1000.size());
-//                try {
-//                    HighGui.imshow(i + "", g1000.submat(rect.boundingRect()));
-//                } catch (Exception e) {
-//
-//                }
-//            }
-//        }
+        getKandidatenFuerAltimeter(g1000TextOptimiert).forEach(rectDoublePair -> {
+            Imgproc.rectangle(g1000, rectDoublePair.p1, Helper.BLACK, -1);
+            Imgproc.putText(g1000, rectDoublePair.p2 + "", new Point(rectDoublePair.p1.x, rectDoublePair.p1.y + rectDoublePair.p1.height - 2), 0, 0.5, Helper.WHITE);
 
-        System.out.println(textDedection.doOCRNumbers(g1000.submat(altimeter)));
+        });
 
+        val sorted = getKandidatenFuerAltimeter(g1000TextOptimiert);
+        double xPP = Math.abs(sorted.get(3).p2 - sorted.get(0).p2) / (Math.abs(sorted.get(3).p1.y + (sorted.get(3).p1.height * 0.5)) - (sorted.get(0).p1.y + (sorted.get(0).p1.height * 0.5)));
+        double x = sorted.get(0).p2 + (xPP * (centerLine - sorted.get(0).p1.y - sorted.get(0).p1.height * 0.5));
 
+        System.out.println("Altimeter " + x);
         HighGui.imshow("", g1000);
         HighGui.waitKey();
     }
 
-    public static Pair<Integer, Mat> getBestThreshAndMat(Mat src) {
+    /**
+     * Sortierte Rückgabe
+     *
+     * @param mat
+     * @return
+     */
+    public List<Pair<Rect, Double>> getKandidatenFuerAltimeter(Mat mat) {
+        val numbers = getAllNumbersOfArea(mat, altimeterUP).stream().filter(rectDoublePair -> rectDoublePair.p2 % 100 == 0).collect(Collectors.toList());
+        numbers.addAll(getAllNumbersOfArea(mat, altimeterDOWN).stream().filter(rectDoublePair -> rectDoublePair.p2 % 100 == 0).collect(Collectors.toList()));
+//        return numbers.stream().sorted((o1, o2) -> o2.p2.compareTo(o1.p2)).findFirst().get();
+        return numbers.stream().sorted(Comparator.comparing(o -> o.p2)).collect(Collectors.toList());
+    }
+
+
+    private List<Pair<Rect, Double>> getAllNumbersOfArea(Mat src, Rect area) {
+        return textDedection.getTextAreasWithTess(src.submat(area)).stream().map(rotatedRect -> {
+            Rect rect = new Rect(new Point(rotatedRect.boundingRect().x + area.x, rotatedRect.boundingRect().y + area.y),
+                    (new Point(rotatedRect.boundingRect().x + area.x + rotatedRect.boundingRect().width, rotatedRect.boundingRect().y + area.y + rotatedRect.boundingRect().height)));
+            return new Pair<>(rect, parseDoubleOrNAN(textDedection.doOCRNumbers(src.submat(rect))));
+        }).collect(Collectors.toList());
+    }
+
+    public Pair<Integer, Mat> getBestThreshAndMat(Mat src) {
         Mat w = new Mat();
         int thresh = 200;
         int nmbrsCount = 0;
         boolean isReady = false;
-//        List<Pair<Integer,Pair<Integer, Mat>>> all = new ArrayList<>();
-//        for (int i = 100; i < 200; i+=5) {
-//            Imgproc.threshold(src, w, thresh, 255, Imgproc.THRESH_BINARY);
-//            Core.bitwise_not(w, w);
-//            val numbers = textDedection.doOCRNumbers(w).split("\n");
-//            all.add(new Pair<>(numbers.length,new Pair<>(thresh, w.clone())));
-//
-//        }
-//        all.sort((o1, o2) -> o2.p1 - o1.p1);
-//
-//        return all.get(0).p2;
 
         Pair<Integer, Mat> out = null;
         do {
             Imgproc.threshold(src, w, thresh, 255, Imgproc.THRESH_BINARY);
             Core.bitwise_not(w, w);
-            val numbers = textDedection.doOCRNumbers(w).split("\n");
-            System.out.println(numbers.length);
-            if (nmbrsCount < numbers.length) {
-                nmbrsCount = numbers.length;
+
+            val altimeter = getKandidatenFuerAltimeter(w).stream().sorted((o1, o2) -> o2.p2.compareTo(o1.p2)).collect(Collectors.toList());
+
+            HighGui.imshow("", w);
+            HighGui.waitKey(100);
+            altimeter.forEach(rectDoublePair -> System.out.println(rectDoublePair.p2));
+            System.out.println();
+            if (altimeter.size() >= 4 && altimeter.get(0).p2 - altimeter.get(1).p2 == 100 && altimeter.get(2).p2 - altimeter.get(3).p2 == 100) {
                 out = new Pair<>(thresh, w.clone());
-            } else if (nmbrsCount > numbers.length) {
                 isReady = true;
             }
-            thresh -= 10;
+//            if (nmbrsCount < numbers.length) {
+//                nmbrsCount = numbers.length;
+//                out = new Pair<>(thresh, w.clone());
+//            } else if (nmbrsCount > numbers.length) {
+//                isReady = true;
+//            }
+            thresh -= 5;
         } while (!isReady);
         return out;
 
-    }
-
-    public static Mat transformieren(Mat mat, RotatedRect ellipse, Size size) {
-        Point[] pts = new Point[4];
-        ellipse.points(pts);
-        return transformieren(mat, Arrays.stream(pts).collect(Collectors.toList()), size);
     }
 
     public static Mat transformieren(Mat mat, List<Point> corner, Size size) {
@@ -134,5 +142,13 @@ public class GarminG1000 {
         Imgproc.warpPerspective(mat, proj, trans, size);
 
         return proj;
+    }
+
+    public static Double parseDoubleOrNAN(String string) {
+        try {
+            return Double.parseDouble(string);
+        } catch (Exception e) {
+            return Double.NaN;
+        }
     }
 }
